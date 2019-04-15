7b87123e0352bcff427ec2d827ffb0901c127e5e
ArseniyKalabin <a.m.kalabin@gmail.com>
2019-04-15 20:02:59 +0300

    added burger  buil controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,23 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(inKeys => {
+            return [...Array(props.ingredients[inKeys])].map((_, i) => {
+                return <BurgerIngredient key={inKeys + i} type={inKeys} />;
+            });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add some ingredients</p>;
+    }
+
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
