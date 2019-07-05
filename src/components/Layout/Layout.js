6b0c50c43812fff23bb4973ff68cde44bd1eb674
ArseniyKalabin <a.m.kalabin@gmail.com>
2019-07-05 18:55:25 +0300

    added sidedrawer logic and menu styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary/Auxiliary';
 import styles from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import { TLSSocket } from 'tls';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={styles.Container}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    showSideDrawerHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    toggleSideDrawerHandler = () => {
+        this.setState(
+            (prevState) => {
+                return { showSideDrawer: !prevState.showSideDrawer };
+            });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.toggleSideDrawerHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.showSideDrawerHandler} />
+                <main className={styles.Container}>
+                    {this.props.children}
+                </main>
+            </Aux >
+        );
+    }
+}
+
+export default Layout;
