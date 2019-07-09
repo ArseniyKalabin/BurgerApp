68419303cb521d4ef648020ea5faa1ce8a883dc2
ArseniyKalabin <a.m.kalabin@gmail.com>
2019-07-09 18:27:08 +0300

    improve app perfomance by adding a shouldComponentUpdate method in Modal component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './OrderSummary.module.css';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 
-const orderSummary = (props) => {
-
-    const ingredientsSummary = Object.keys(props.ingredients)
-        .map(inKeys => {
-            return (
-                <li key={inKeys}>
-                    <span style={{ textTransform: 'capitalize' }}>{inKeys}</span>: {props.ingredients[inKeys]}
-                </li>
-            );
-        });
-
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p className={styles.Bold}>Total Price: {props.price.toFixed(2)}</p>
-            <p>Continue to checkout?</p>
-            <Button btnType='Danger' clicked={props.canlelPurchase}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.continuePurchase}>CONTINUE</Button>
-        </Aux>
-    );
+class OrderSummary extends Component {
+    //This could be a functional component
+    componentWillUpdate() {
+        console.log('[orderSummary componentWillUpdate]');
+    }
+    render() {
+        const ingredientsSummary = Object.keys(this.props.ingredients)
+            .map(inKeys => {
+                return (
+                    <li key={inKeys}>
+                        <span style={{ textTransform: 'capitalize' }}>{inKeys}</span>: {this.props.ingredients[inKeys]}
+                    </li>
+                );
+            });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p className={styles.Bold}>Total Price: {this.props.price.toFixed(2)}</p>
+                <p>Continue to checkout?</p>
+                <Button btnType='Danger' clicked={this.props.canlelPurchase}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.continuePurchase}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
 
